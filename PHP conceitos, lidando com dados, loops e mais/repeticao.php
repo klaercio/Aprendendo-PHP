5d4252a30ea1f11184574510d3d55dbe8c4d059e
Klaercio <klaerciosilva@gmail.com>
2022-07-22 22:50:08 -0300

    🚀Trabalhando com estruturas de repetições em PHP

---
+++ PHP conceitos, lidando com dados, loops e mais/repeticao.php
@@ -0,0 +1,12 @@
+<?php
+    for($numero = 1; $numero <= 10; $numero++) {
+        echo "numero $numero" .PHP_EOL;
+    }    
+
+    $num = 1;
+
+    while($num <= 10) {
+        echo "numero do while $num" .PHP_EOL;
+        $num++;
+    }
+?>
